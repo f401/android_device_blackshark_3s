fdcacbdfb915959cc2b1737775c09255d6ae4bbc
f401 <nanLianQing@outlook.com>
2024-01-02 00:55:20 +0800

    Update

--- twrp_klein.mk
+++ twrp_klein.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 PRODUCT_DEVICE := klein
-PRODUCT_NAME := twrp_klein
+PRODUCT_NAME := twrp_blackshark_klein
 PRODUCT_BRAND := blackshark
 PRODUCT_MODEL := SHARK KLE-A0
 PRODUCT_MANUFACTURER := blackshark
